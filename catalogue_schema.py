d9ddb0922670177043977c8337fe21ff72f2b4d8
nobac3 <antoinecabon3@gmail.com>
2020-01-27 11:04:30 +0100

    catalogue_schema

---
+++ catalogue_schema.py
@@ -0,0 +1,15 @@
+from app import ma
+
+class CatalogueSchema(ma.Schema):
+    class Meta:
+        # Fields to expose
+        fields = ("id", "messier", "ngc", "object_type", "season", "magnitude", "constellation_eng", "constellation_fr", "constellation_lat", "right_ascension", "declinaison", "distance", "size", "discoverer", "year", "image_URL", "constellation")
+
+    # Smart hyperlinking
+    _links = ma.Hyperlinks(
+        {"self": ma.URLFor("detail", id="<id>")}
+    )
+
+
+catalogue_schema = CatalogueSchema()
+catalogues_schema = CatalogueSchema(many=True)

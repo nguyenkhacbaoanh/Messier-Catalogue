4e1cc5e74b2c39e784834cd784f8afaa11e5d205
nobac3 <antoinecabon3@gmail.com>
2020-01-28 11:00:32 +0100

    decalage de column

--- catalogue.py
+++ catalogue.py
@@ -2,7 +2,6 @@ from app import db
 
 class Catalogue(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    messier = db.Column(db.String(120), unique=True, nullable=False)
     ngc = db.Column(db.String(100), unique=True, nullable=False)
     object_type = db.Column(db.String(100), unique=True, nullable=False)
     season= db.Column(db.String(100), unique=True, nullable=False)
@@ -17,4 +16,5 @@ class Catalogue(db.Model):
     discoverer = db.Column(db.String(100), unique=True, nullable=False)
     year = db.Column(db.String(100), unique=True, nullable=False)
     image_URL = db.Column(db.String(500), unique=True, nullable=False)
+    image_URL1 = db.Column(db.String(500), unique=True, nullable=False)
     constellation = db.Column(db.String(100), unique=True, nullable=False)
--- catalogue_schema.py
+++ catalogue_schema.py
@@ -3,7 +3,7 @@ from app import ma
 class CatalogueSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ("id", "messier", "ngc", "object_type", "season", "magnitude", "constellation_eng", "constellation_fr", "constellation_lat", "right_ascension", "declinaison", "distance", "size", "discoverer", "year", "image_URL", "constellation")
+        fields = ("id", "ngc", "object_type", "season", "magnitude", "constellation_eng", "constellation_fr", "constellation_lat", "right_ascension", "declinaison", "distance", "size", "discoverer", "year", "image_URL", "image_URL1" ,"constellation")
 
     # Smart hyperlinking
     _links = ma.Hyperlinks(

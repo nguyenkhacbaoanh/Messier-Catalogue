84c7bb972a07e4d515a202d01ca82561abea77b8
nguyenkhacbaoanh <khacbaoanh.nguyen@gmail.com>
2020-02-01 23:49:14 +0100

    add CORS app

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 from app.main.config import config_by_name
 
@@ -11,4 +11,9 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
+    CORS(
+        app,
+        resources={r"/api/v1/*": {"origins": "127.0.0.1"}},
+        headers=['Content-Type', 'X-Requested-With', 'Authorization']
+    )
     return app

802ac447f167ffd5d3fc1e5b0482be9331b48df1
nguyenkhacbaoanh <khacbaoanh.nguyen@gmail.com>
2020-02-26 11:10:06 +0100

    web application for messier catalogue

---
+++ web/app/src/searchBar/Api.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react';
+import InputBase from '@material-ui/core/InputBase';
+
+class Search extends Component {
+    // constructor(props) {
+    //   super(props)
+    // }
+    token = null;
+    state = {
+      query: "",
+      messier: []
+    };
+    onChange = e => {
+      const { value } = e.target;
+      this.setState({
+        query: value
+      });
+  
+      this.search(value);
+    };
+  
+    search = query => {
+      const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const url = `https://anh.pythonanywhere.com/api/v2/messier-catalogue/${query}`;
+      const token = {};
+      this.token = token;
+  
+      fetch(proxyurl + url)
+        .then(results => results.json())
+        .then(dt => {
+          if (this.token === token) {
+              console.log(dt)
+            this.setState({ messier: dt.data });
+          }
+        }).catch((e) => {console.log(e)});
+    };
+  
+    componentDidMount() {
+      this.setState({
+        classes: this.props
+      });
+      console.log(this.props);
+      this.search("");
+    }
+  
+    render() {
+      const { classes } = this.props;
+      return (
+        // <form>
+        //   <input
+        //     type="text"
+        //     className="search-box"
+        //     placeholder="Search for..."
+        //     onChange={this.onChange}
+        //   />
+        //   {this.state.messier.map(mes => (
+        //     <ul key={mes.id}>
+        //       <li>{mes.id}</li>
+        //     </ul>
+        //   ))}
+        // </form>
+        <InputBase
+          placeholder="Search…"
+          // onChange={handleInputSearchOnChange}
+          classes={{
+            root: {color: 'inherit'},
+            input: classes.inputInput,
+          }}
+          inputProps={{ 'aria-label': 'search' }}
+          onChange={this.onChange}
+        />
+      );
+    }
+  }
+
+  export default Search;
